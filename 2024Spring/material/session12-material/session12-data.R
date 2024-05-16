1897a5c36a42394edde631d4c98ea73c67502315
Claudius Graebner-Radkowitsch <claudius@claudius-graebner.com>
2024-05-16 11:59:58 +0200

    Update session 12, prepare solutions (not yet linked)

--- 2024Spring/material/session12-material/session12-data.R
+++ 2024Spring/material/session12-material/session12-data.R
@@ -14,9 +14,10 @@ education_data_raw <- WDI::WDI(
 
 education_data <- education_data_raw %>% 
   select(iso3c, year, EducationSpending, GDPpc, income) %>% 
-  filter(income!="Aggregates")
+  filter(income!="Aggregates", income!="Not classified")
 
-fwrite(education_data, here("2024Spring/material/session12-material/education_income.csv"))
+fwrite(education_data, 
+       here("2024Spring/material/session12-material/education_income.csv"))
 
 # Child mortality------------
 child_mortality_raw <- WDI::WDI(
@@ -27,9 +28,10 @@ child_mortality_raw <- WDI::WDI(
   extra = TRUE)
 
 child_mortality <- child_mortality_raw %>% 
-  filter(income!="Aggregates") %>% 
+  filter(income!="Aggregates", income!="Not classified") %>% 
   select(iso3c, year, ChildMortality, GDPpc)
  
-fwrite(education_data, here("2024Spring/material/session12-material/child_mortality.csv"))
+fwrite(child_mortality, 
+       here("2024Spring/material/session12-material/child_mortality.csv"))
 
   
